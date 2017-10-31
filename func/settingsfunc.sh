bffc2d946a92393c38122765d710171b8ce30d0e
Cj-bc <cj-bc@y-modify.org>
2017-10-31 14:30:18 +0900

    set the act when it's called with specific arguments(set,send,channel,user,c and u(as shortcut of channel,user)

--- func/settingsfunc.sh
+++ func/settingsfunc.sh
@@ -5,8 +5,9 @@ source texts/FunctionsTexts.sh   #All texts are in here.
 function setup {
 
  if [ $1 -eq "first" ]
+ then
   echo $AnnounceToGetToken
-  
-  
+  read Token
+ fi
 
-} 
+}
--- slacker.sh
+++ slacker.sh
@@ -5,6 +5,7 @@
 # version 0.0.1
 Version=0.0.1
 source ./func/settingfunc.sh
+source ./texts/common Texts.sh
 
 #test wheather initialization is in need
 if [ $1 -eq "init" ] || [ -f .basicconf ] && [ $# -ne 0 ]
@@ -14,6 +15,22 @@ then
 fi
 
 # with no arg,show version
-if [ $# -eq 1 ]
+if [ $# -eq 0 ]
 then
  echo "version: ${Version}"
+fi
+
+# test all arg and call function
+# Functions are here:
+# settingfunc.sh --- setting
+#                 |- setting_channel
+#                 |- setting_user
+# sendfunc.sh --- send
+
+case "$1" in
+ "set" ) shift;setting "$＊" ;;
+ "send" | "s" )  shift;send "$＊" ;;
+ "channel" | "c" ) shift;setting_channel "$＊" ;;
+ "user" | "u" ) shift;setting_user "$＊" ;;
+ ＊ ) echo "unknown argument.\n${usage}"
+esac
