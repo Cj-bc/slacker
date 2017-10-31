91a2c30ad2a26808acd774ff41b7e330025743ec
Cj-bc <cj-bc@y-modify.org>
2017-10-31 12:44:06 +0900

    Now slacker return its version when it's called with no arg,

--- slacker.sh
+++ slacker.sh
@@ -1,13 +1,19 @@
-# Written by Cj in y-modify
-#usege: 
-#slack set token <token_code> ---> set token
+#!bin/bash
 
+# Written by Cj in y-modify
+# 
+# version 0.0.1
+Version=0.0.1
 source ./func/settingfunc.sh
 
-#test wheather initializing is in need
+#test wheather initialization is in need
 if [ $1 -eq "init" ] || [ -f .basicconf ] && [ $# -ne 0 ]
 then
  echo "you need to setup your slacker first."
  setup first      #this is in settingfunc.sh file.
-fi 
- 
+fi
+
+# with no arg,show version
+if [ $# -eq 1 ]
+then
+ echo "version: ${Version}"
