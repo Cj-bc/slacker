bc7d4e53da6940261d155440d52e394eafbe631c
Cj-bc <cj-bc@y-modify.org>
2017-11-01 22:02:34 +0900

    Start making AnalyzeId.GetUserId()

--- func/AnalyzeId.sh
+++ func/AnalyzeId.sh
@@ -17,5 +17,18 @@ function GetChannelId {
 
 function GetUserId {
 
+ UserName=$1
+
+
+#
+# 1.Get all userData
+# 2.Grep that file to get id of $UserName
+# 3.Return the UserId
+#
+
+# 1.
+ curl -d "token=${Token}" -o .data "https://slack.com/api/users.list"
+# 2.
+ grep 
 
 }
