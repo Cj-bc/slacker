83e13240bab8e7bcbd3096ca523e5d3cc66cb9fe
Cj <cj-bc@y-modify.org>
2017-11-04 20:38:11 +0900

    Make contents of GetUserId()

--- func/AnalyzeId.sh
+++ func/AnalyzeId.sh
@@ -2,9 +2,12 @@
 ##############
 # making
 ##############
-
-source .basicconf
-
+#
+# Using files
+#  * .basicconf
+#  * texts/FunctionsTexts.sh
+#  * texts/URLs.sh
+#  * texts/Error_Codes.sh
 
 function GetChannelId {
 
@@ -23,13 +26,22 @@ function GetUserId {
 
 #
 # 1.Get all userData
-# 2.Grep that file to get id of $UserName
-# 3.Return the UserId
+# 2.Use awk to collect needed datas(UserId and username)
+# 3.Check whether designated name is in the user list
+# 4.Select needed UserId and name
+# 5.Return the UserId
 #
 
-# 1. -o option
- curl -d "token=${Token}" -o .data "https://slack.com/api/users.list"
-# 2.
- 
-
+# 1. -o option output the response
+ curl -d "token=${Token}" -o .data $UserListURL
+# 2. 
+ awk -v username=$UserName -F "\"" ' /"id"/ { print $4 } /"name"/ { print $4;if ( $4 == username ) exit } ' ./.data > .data  
+# 3.
+ if `cat .data`| grep ${UserName}
+ then
+  :
+ else
+  echo $NoExistUserName
+  exit $Error_NoExistUsrName
+ fi 
 }
