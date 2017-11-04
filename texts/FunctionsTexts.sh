b47201aef60e59ee06482fd3b52577882ab9bfc7
Cj <cj-bc@y-modify.org>
2017-11-04 20:38:54 +0900

    Add texts for AnalyzeId.sh.

--- texts/FunctionsTexts.sh
+++ texts/FunctionsTexts.sh
@@ -6,3 +6,10 @@ URL_GetToken="https://slack.com/api/"
 #texts
 AnnounceToGetToken="you need to setup slacker first\nFrom here, \n${URL_GetToken}\nPlease get a token,and type it here.\nif you don't know how to do,this file will help you\n${GuideToGetToken}"
 
+
+#
+# For AnalyzeId.sh
+#
+
+NoExistUserName="Sorry,But that user does not exist.Don't you make spell mistakes?\nPlease set username again."
+
