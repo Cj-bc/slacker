2f17e89debdd0dd68e01e58498cbc8f2e0fd817c
Cj <cj-bc@y-modify.org>
2017-11-12 18:56:17 +0900

    Add errorsfunc.ErrorCheckHTTP()

--- func/Errorfunc.sh
+++ func/Errorfunc.sh
@@ -24,5 +24,15 @@ function ErrorCheckAPI() {
 
 function ErrorCheckHTTP() {
 
- 
+ case $1 in
+  200) return 0 ;;
+  301) echo $HTTP_301 1>&2;;
+  400) echo $HTTP_400 1>&2;;
+  404) echo $HTTP_404 1>&2;;
+  408) echo $HTTP_408 1>&2;;
+  501) echo $HTTP_501 1>&2;;
+  503) echo $HTTP_503 1>&2;;
+  505) echo $HTTP_505 1>&2;;
+ esac
+  return $Error_HTTP 
 }
--- texts/FunctionsTexts.sh
+++ texts/FunctionsTexts.sh
@@ -12,3 +12,17 @@ AnnounceToGetToken="you need to setup slacker first\nFrom here, \n${URL_GetToken
 
 NoExistUserName="Sorry,But that user does not exist.Don't you make spell mistakes?\nPlease set username again."
 NoExistChannelName="Sorry,But that channel does not exist.Don't you make spell mistakes?\nPlease set channelname again."
+
+
+
+#
+# For Errorsfunc.sh
+#
+
+HTTP_301="301 Moved Permanently\nAPI's URL seems to be changed.Please serch latest URL for the method,then replace older URL in text/URLs.sh."
+HTTP_400="400 Bad Request\nThe method could no longer exist.(I'm not sure.) please check whether the URL in text/URLs.sh is correct"
+HTTP_404="404 Not Found\nThe method might no longer exist now.please check whether the URL in text/URLs.sh is correct."
+HTTP_408="408 Request Timeout\nPlease try again."
+HTTP_501="501 Not Implemented\nWas that gone away!? Serch for the same-work method.Please tell me later ;)"
+HTTP_503="503 Service Unavaliable\nslack system itself seems to be downed.Please try later."
+HTTP_505="505 HTTP Version Not Supported\nPlease add curl option \"--http1.1\"or\"--http2\"which slack.com supports."
