26ade521194dc29e5aa12c85096416ec5a71e02d
Cj <cj-bc@y-modify.org>
2017-11-18 15:30:06 +0900

    Delete unwanted space.it has caused error.

--- texts/URLs.sh
+++ texts/URLs.sh
@@ -14,7 +14,7 @@ GuideToGetToken="./texts/guides/GetToken.txt"
 # For AnalyzeId.sh
 #
 UserListURL="https://slack.com/api/users.list"
-ImListURL= "https://slack.com/api/im.list"
+ImListURL="https://slack.com/api/im.list"
 ImOpenURL="https://slack.com/api/im.open"
 ChannelListURL="https://slack.com/api/channels.list"
 
