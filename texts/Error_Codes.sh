f6a049eab8fea5d3077191f0994e2b035b4cf224
Cj <cj-bc@y-modify.org>
2017-11-19 09:46:31 +0900

    Add temporary errorcodes. I SHOULD change it later

--- texts/Error_Codes.sh
+++ texts/Error_Codes.sh
@@ -2,9 +2,9 @@
 # Here're all error codes slacker would return.
 # if it succeed, it return 0
 
-
-Error_NoExistUserName=
-Error_NoExistChannelName=
-Error_HTTP=
-Error_API=
-Error_NotEnoughArguments=
+# this is just temporary value. I need to change it
+Error_NoExistUserName=1
+Error_NoExistChannelName=2
+Error_HTTP=3
+Error_API=4
+Error_NotProperArguments=5
