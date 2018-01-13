41ea2a0e723c8211d40dfd6f8db270c0269192cf
Cj <cj-bc@y-modify.org>
2018-01-13 14:32:16 +0900

    add version info in fomulae

--- slacker.rb
+++ slacker.rb
@@ -2,6 +2,7 @@ class Slacker < Formula
   desc "Allows to send slack messages from terminal"
   homepage "https://github.com/Cj-bc/slacker"
   url "https://github.com/Cj-bc/slacker/archive/v0.1.tar.gz"
+  version 0.1
   sha256 "8e6081a851df2fbda8f4d7feb0ee4cd3855da7d8c81c9769948ae7633c6aefb6"
   head "https://github.com/Cj-bc/slacker.git"
 
