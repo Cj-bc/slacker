164fd6530239b69fc0e52616f6a1ca6ddcdc45aa
Cj <cj-bc@y-modify.org>
2018-01-13 14:21:15 +0900

    set SHA value to collect one

--- slacker.rb
+++ slacker.rb
@@ -2,7 +2,7 @@ class Slacker < Formula
   desc "Allows to send slack messages from terminal"
   homepage "https://github.com/Cj-bc/slacker"
   url "https://github.com/Cj-bc/slacker/archive/v0.1.tar.gz"
-  sha256 "c8e0e14f377637d4441095b9a14fc613ffaad03958b3feb2ffe0545ee1aba304"
+  sha256 "8e6081a851df2fbda8f4d7feb0ee4cd3855da7d8c81c9769948ae7633c6aefb6"
   head "https://github.com/Cj-bc/slacker.git"
 
   depends_on "awk"
