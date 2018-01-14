d7cd8b2d6dbdebdd3c8b29e87a0be019ba6ec181
Cj <cj-bc@y-modify.org>
2018-01-14 23:26:35 +0900

    ver.0.1.2.1. tag

--- slacker.sh
+++ slacker.sh
@@ -6,10 +6,10 @@
 # 
 # This software is released under MIT License.
 # http://opensource.org/license/mit-license.php
-# version 0.1.2
+# version 0.1.2.1
 
 # source textfiles,functions
-Version=0.1.2
+Version=0.1.2.1
 
 shopt -s xpg_echo
 
