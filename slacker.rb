d7e07025decc14889dc19b1bbacfe85ed4c93666
Cj <cj-bc@y-modify.org>
2018-01-21 00:52:56 +0900

    rewrite url's repo name (slacker->homebrew-slacker)

--- slacker.rb
+++ slacker.rb
@@ -1,10 +1,10 @@
 class Slacker < Formula
   version = "0.1.2.3"
   desc "Allows to send slack messages from terminal"
-  homepage "https://github.com/Cj-bc/slacker"
-  url "https://github.com/Cj-bc/slacker/archive/v" + version + ".tar.gz"
+  homepage "https://github.com/Cj-bc/homebrew-slacker"
+  url "https://github.com/Cj-bc/homebrew-slacker/archive/v" + version + ".tar.gz"
   sha256 "1da848f5f5195992f8b973e1600b9eee8d056da867fe90be66c42a10a9cc91e1"
-  head "https://github.com/Cj-bc/slacker.git"
+  head "https://github.com/Cj-bc/homebrew-slacker.git"
 
   depends_on "awk"
   depends_on "python"
