26d3164738e83be34289abe617ee0646ce3fbc1d
Cj <cj-bc@y-modify.org>
2018-01-27 10:14:37 +0900

    Remove unwanted quotes

--- slacker.rb
+++ slacker.rb
@@ -3,7 +3,7 @@ class Slacker < Formula
   desc "Allows to send slack messages from terminal"
   homepage "https://github.com/Cj-bc/homebrew-slacker"
   url "https://github.com/Cj-bc/homebrew-slacker/archive/v" + version + ".tar.gz"
-  sha256 "4ebc59cf0cd9208bbca3f484b5fda51210495eb174dfb7c9bcc032bf80123283""
+  sha256 "4ebc59cf0cd9208bbca3f484b5fda51210495eb174dfb7c9bcc032bf80123283"
   head "https://github.com/Cj-bc/homebrew-slacker.git"
 
   depends_on "awk"
