9469af511276785ac8b1ba38380466bfabb9caee
Cj <cj-bc@y-modify.org>
2018-01-27 11:09:58 +0900

    Change hash for v0.1.2.6

--- slacker.rb
+++ slacker.rb
@@ -3,7 +3,7 @@ class Slacker < Formula
   desc "Allows to send slack messages from terminal"
   homepage "https://github.com/Cj-bc/homebrew-slacker"
   url "https://github.com/Cj-bc/homebrew-slacker/archive/v" + version + ".tar.gz"
-  sha256 "fde2b38c97fe126ae6baee888160dd3b752ef8ae8bdcf623c7ac512d209025ac"
+  sha256 "44d9d960af4179db3953e5885f45d561451859cc0fad24cb8f4496e35e9cb7ab"
   head "https://github.com/Cj-bc/homebrew-slacker.git"
 
   depends_on "awk"
