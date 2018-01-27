b384942a58f1e0adff31eb0d4347c970b27cbc93
Cj <cj-bc@y-modify.org>
2018-01-27 09:02:42 +0900

    formulae is now almost re-genarated using formula's methods
    use formula's method(i.e. prefix.install,ohai)

--- slacker.rb
+++ slacker.rb
@@ -1,6 +1,3 @@
-require 'fileutils' # for fileutils.mv
-
-
 class Slacker < Formula
   version = "0.1.2.4"
   desc "Allows to send slack messages from terminal"
@@ -12,25 +9,14 @@ class Slacker < Formula
   depends_on "awk"
   depends_on "python"
 
-  def install 
-
-	dest = "%x(brew --prefix)/Cellar/slacker/#{version}"	 # for mv libs/confs
-	system "mv","slacker.sh","slacker" # rename
-	bin.install "slacker" # make link
-	mkdir("#{dest}") unless File.exist?("#{dest}")	# make dir of this version
-	mv(['func/','texts/'],"#{dest}")
-	system "mv",".sourcefiles","texts/templates/.slackerconf","#{dest}"
-	  #    FileUtils.mv(origin_file,target_file)
-	  #    FileUtils.cp_r([origin_file1,origin_file2],target_directory)
-	# homebrew_prefix = "#{HOMEBREW_PREFIX}" # Get $HOMEBREW_PREFIX
-	# mv('slacker.sh','slacker') # rename slacker.sh
-	# bin.install "slacker"	# install slacker
-	# mkdir_p("#{homebrew_prefix}/Cellar/slacker/#{version}/")
-	# cp_r(['func/','texts/'],"#{homebrew_prefix}/Cellar/slacker/#{version}/")
-	# cp_r(['.sourcefiles','.slackerconf'],"#{homebrew_prefix}/Cellar/slacker/") unless File.file?("#{homebrew_prefix}/.slackerconf")
-  end
+ def install 
+	  ohai "Renaming executable..."
+	  system "mv" "slacker.sh" "slacker" # Rename
+	  bin.install "slacker" # Install slacker
+	  prefix.install Dir[func] Dir[texts] # Install all texts and functions
+ end
 
   test do
-    system "/bin/bash slacker"
+    system "slacker"
   end
 end
