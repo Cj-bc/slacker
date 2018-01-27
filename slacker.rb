b42ad2935f3326682a26bf85c62beb476c0a4402
Cj <cj-bc@y-modify.org>
2018-01-27 09:59:52 +0900

    Fix Dir[]

--- slacker.rb
+++ slacker.rb
@@ -13,8 +13,7 @@ class Slacker < Formula
 	  ohai "Renaming executable..."
 	  system "mv " "slacker.sh " "slacker" # Rename
 	  bin.install "slacker" # Install slacker
-	  prefix.install Dir[func]
-	  prefix.install Dir[texts] # Install all texts and functions
+	  prefix.install Dir["func"], Dir["texts"] # Install all texts and functions
  end
 
   test do
